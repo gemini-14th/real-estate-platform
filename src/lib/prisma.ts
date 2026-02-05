550f2a03ce7ada931cd3a46df38c8c01bf9ee9b4
Samuel Anindo <samuelanindo14@gmail.com>
2026-02-05 12:45:06 +0300

    upgrade6

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,8 +10,8 @@ export const prisma =
         log: ['error'],
     });
 
-if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     globalForPrisma.prisma = prisma;
+// }
 
 export default prisma;
